2e4c94ec444650364d0621dfc6f9f7ef58d0ed73
ahmadshawaheen98 <ahmadshawaheem7@gmail.com>
2022-12-02 15:29:18 +0300

    add file

---
+++ EpplusExcel/Migrations/20221201183842_initDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EpplusExcel.Migrations
+{
+    public partial class initDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "people",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    email = table.Column<string>(nullable: true),
+                    isActive = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_people", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "people");
+        }
+    }
+}
---
+++ EpplusExcel/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+namespace EpplusExcel
+{
+    public class person
+    {
+        
+        public int id { set; get; }
+        public string Name { set; get; }
+
+        public string email { set; get; }
+
+        public string isActive { set; get; }
+
+    }
+    public class context : DbContext
+    {
+        public DbSet<person> people { set; get; }
+   
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=localhost; Initial catalog=UserExcal; Integrated security=true ");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+    class Program
+    {
+       // private static object strarray;
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome");
+            string file =Console.ReadLine();
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(file)))
+            {
+               // ExcelPackage.LicenseContext = LicenseContext.Commercial;
+               ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                var sheet = package.Workbook.Worksheets["data"];
+                var persons = new Program().GetList<person>(sheet);
+            }
+        }
+        private List<T> GetList<T>(ExcelWorksheet sheet)
+        {
+            List<T> ts = new List<T>();
+            var columninfo = Enumerable.Range(1, sheet.Dimension.Columns).ToList().Select(n =>
+            new { index = n, columnName=sheet.Cells[1,n].Value.ToString()}
+            );
+       
+                
+            for (int row=2; row<=sheet.Dimension.Rows; row++)
+            {
+                T obj = (T)Activator.CreateInstance(typeof(T));
+                foreach(var prop in typeof(T).GetProperties())
+                {
+                    int col = columninfo.SingleOrDefault(c => c.columnName == prop.Name).index;
+                    var val = sheet.Cells[row, col].Value;
+                    var proptype = prop.PropertyType;
+                    prop.SetValue(obj, Convert.ChangeType(val, proptype));
+
+                   
+                }
+
+                context context = new context();
+                context.Add(obj);
+                context.SaveChanges();
+                //ts.Add(obj);
+            }
+        
+            return ts;
+            
+        }
+    }
+   
+}
